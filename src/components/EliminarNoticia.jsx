600a0033ded7ad83bd7b554867becde073505e0b
Samantha Rizo <71712361+SamanthaRizo@users.noreply.github.com>
2025-11-11 00:47:21 -0600

    Update EliminarNoticia.jsx

--- src/components/EliminarNoticia.jsx
+++ src/components/EliminarNoticia.jsx
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
 import { deleteNoticia } from '../api';
-import { useNavigate } from 'react-router-dom';
 
 function EliminarNoticia() {
   const [idEliminar, setIdEliminar] = useState('');
   const [mensaje, setMensaje] = useState('');
   const [error, setError] = useState('');
-  const navigate = useNavigate();
 
   const handleEliminar = async () => {
     try {
       const data = await deleteNoticia(idEliminar);
-      setMensaje(data.mensaje || data.message);
+      setMensaje(data.mensaje);
       setError('');
       setIdEliminar('');
     } catch (err) {
@@ -25,11 +23,9 @@ function EliminarNoticia() {
       <h2>Eliminar Noticia</h2>
       <input type="number" placeholder="ID Noticia" value={idEliminar} onChange={(e) => setIdEliminar(e.target.value)} />
       <button className="danger" onClick={handleEliminar}>Eliminar</button>
-
+      <button className="secondary" onClick={() => window.history.back()}>Volver al menú</button>
       {mensaje && <p style={{ color: 'green' }}>{mensaje}</p>}
       {error && <p style={{ color: 'red' }}>{error}</p>}
-
-      <button className="secondary" onClick={() => navigate('/menu')}>Volver al menú</button>
     </div>
   );
 }
