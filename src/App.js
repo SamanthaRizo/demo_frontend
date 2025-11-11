21cfeaa1d0b9a522f908d20ce11ea8a1a3816f13
Samantha Rizo <71712361+SamanthaRizo@users.noreply.github.com>
2025-11-10 22:44:12 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
+
 import MenuPage from './components/MenuPage';
-import { AuthProvider } from './contexts/AuthContext';
-import PrivateRoute from './components/PrivateRoute';
 import ListaNoticias from './components/ListaNoticias';
 import CrearNoticia from './components/CrearNoticia';
 import ActualizarNoticia from './components/ActualizarNoticia';
@@ -12,17 +10,14 @@ import EliminarNoticia from './components/EliminarNoticia';
 function App() {
   return (
     <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/menu" element={<PrivateRoute><MenuPage /></PrivateRoute>} />
-          <Route path="/listanoticias" element={<PrivateRoute><ListaNoticias /></PrivateRoute>} />
-          <Route path="/crearnoticia" element={<PrivateRoute><CrearNoticia /></PrivateRoute>} />
-          <Route path="/actualizarnoticia" element={<PrivateRoute><ActualizarNoticia /></PrivateRoute>} />
-          <Route path="/eliminarnoticia" element={<PrivateRoute><EliminarNoticia /></PrivateRoute>} />
-          <Route path="/" element={<Navigate to="/login" />} />
-        </Routes>
-      </AuthProvider>
+      <Routes>
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/listanoticias" element={<ListaNoticias />} />
+        <Route path="/crearnoticia" element={<CrearNoticia />} />
+        <Route path="/actualizarnoticia" element={<ActualizarNoticia />} />
+        <Route path="/eliminarnoticia" element={<EliminarNoticia />} />
+        <Route path="/" element={<Navigate to="/menu" />} />
+      </Routes>
     </Router>
   );
 }
