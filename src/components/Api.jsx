1cc4cec9a4f6544d106eaccff6cae5028d2d1894
Samantha Rizo <71712361+SamanthaRizo@users.noreply.github.com>
2025-11-17 18:00:15 -0600

    Update Api.jsx

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
-const Api = () => (
-  <div className="container">
-    <h2>API - Documentación</h2>
-    <SwaggerUI url="/swagger/noticias.yaml" />
-  </div>
-);
+const Api = () => {
+  return (
+    <div className="container">
+      <h2>Documentación de la API</h2>
+      <SwaggerUI url="/swagger/noticias.yaml" />
+    </div>
+  );
+};
 
 export default Api;
