c4f1ac9bccfad11b45c9f41c839e8d7d9dc30615
Samantha Rizo <71712361+SamanthaRizo@users.noreply.github.com>
2025-11-17 20:12:24 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+
 import MenuPage from "./components/MenuPage";
 import ListaNoticias from "./components/ListaNoticias";
 import CrearNoticia from "./components/CrearNoticia";
 import ActualizarNoticia from "./components/ActualizarNoticia";
 import EliminarNoticia from "./components/EliminarNoticia";
+import VerNoticia from "./components/VerNoticia";   // ← IMPORTANTE
+import Api from "./components/Api";                 // ← IMPORTANTE
 
 function App() {
   return (
@@ -15,6 +18,13 @@ function App() {
         <Route path="/crearnoticia" element={<CrearNoticia />} />
         <Route path="/actualizarnoticia" element={<ActualizarNoticia />} />
         <Route path="/eliminarnoticia" element={<EliminarNoticia />} />
+        <Route path="/noticia/:id" element={<VerNoticia />} />
+
+
+        {/* ← Página Swagger */}
+        <Route path="/api" element={<Api />} />
+
+        {/* Redirección */}
         <Route path="/" element={<Navigate to="/menu" />} />
       </Routes>
     </Router>
@@ -22,3 +32,4 @@ function App() {
 }
 
 export default App;
+
